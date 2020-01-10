7634d2822f7276413f3a6ced7b3416f4a9794dc1
suja <sujijothy@gmail.com>
2020-01-10 00:36:25 -0600

    spa user validation studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -9,31 +9,43 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("add")
     public String  displayAddUserForm(Model model){
-//        model.addAttribute(new User());
+      model.addAttribute(new User());
         return "/user/add";
     }
 
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, Errors errors, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
         // add form submission handling code here
 //        model.addAttribute("user",user);
 //        model.addAttribute("verify", verify);
 //        model.addAttribute("username", user.getUsername());
 //        model.addAttribute("email", user.getEmail());
-        if (verify != null && verify.equals(user.getPassword())) {
-            model.addAttribute("username", user.getUsername());
-            return "/user/index";
-        } else {
-            model.addAttribute("error","Password is not matched");
-            model.addAttribute("username",user.getUsername());
-            model.addAttribute("email", user.getEmail());
-            return "/user/add";
+//        if (verify != null && verify.equals(user.getPassword())) {
+//            model.addAttribute("username", user.getUsername());
+//            return "/user/index";
+//        } else {
+//            model.addAttribute("error","Password is not matched");
+//            model.addAttribute("username",user.getUsername());
+//            model.addAttribute("email", user.getEmail());
+//            return "/user/add";
+
+            if(errors.hasErrors()){
+            //    model.addAttribute("user","user");
+                return "/user/add";
+            }
+            else
+            {
+                model.addAttribute("name", user.getUsername());
+                return "/user/index";
+            }
+
         }
     }
-}
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,17 +1,33 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message="Username can't be empty!")
+    @Size(min = 5, max=15, message="UserName length should be between 5 and 15")
     private String username;
-    private String password;
+    @Email(message="Give Valid Email address!")
     private String email;
-    public User(String username, String password,String email) {
+    @NotBlank(message="Password can't be empty")
+    @Size(min=6, message="Password should contain minimum 6 characters long!")
+    private String password;
+    @NotNull(message="Password do not match")
+    private String verifyPassword;
+    public User() {
+
+    }
+
+    public User(String username, String email, String password, String verifyPassword) {
+        this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword=verifyPassword;
     }
-    public User(){
 
-    }
     public String getUsername() {
         return username;
     }
@@ -33,6 +49,56 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        checkPassword();
+    }
+    private void checkPassword(){
+        if((this.password != null) && (this.verifyPassword != null) && ( (this.password).toLowerCase().equals((this.verifyPassword).toLowerCase()) ))
+            this.verifyPassword = null;
     }
 }
+
+
+
+    //    private String username;
+//    private String password;
+//    private String email;
+//    public User(String username, String password,String email) {
+//        this.username = username;
+//        this.email = email;
+//        this.password = password;
+//    }
+//    public User(){
+//
+//    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
